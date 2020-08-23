0340bd13e99488efecaa104e6dda26911823c994
Yunju Chen <info@yunjuchen.net>
2020-08-22 18:13:38 -0700

    Clear text when submitting todo

--- src/components/AddTodo.js
+++ src/components/AddTodo.js
@@ -1,17 +1,22 @@
 import React, { useState } from 'react'
 import { Button } from 'antd'
 
-function AddTodo(props) {
-  const[text, setText] = useState([])
+function AddTodo({ addTodo }) {
+  const[text, setText] = useState('')
 
   const handleChange = (e) => {
     setText(e.target.value)
   }
 
+  const handleClick = () => {
+    addTodo(text)
+    setText('')
+  }
+
   return (
     <div className="inputbar">
       <input type="text" onChange={handleChange} value={text} placeholder="Add todo item"></input>
-      <Button type="primary" onClick={() => props.addTodo(text)}>Add</Button>
+      <Button type="primary" onClick={handleClick}>Add</Button>
     </div>
   )
 }
