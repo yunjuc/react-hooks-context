ba1a4078f04c0098c5ba257f8a72b6398cbd1e9f
Yunju Chen <info@yunjuchen.net>
2020-08-23 13:29:26 -0700

    Initial commit

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,21 +1,31 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import data from '../data'
 import AddTodo from './AddTodo'
 import Todo from './Todo'
 
+localStorage.setItem('todos', JSON.stringify(data))
+
 function TodoList() {
-  const [todos, setTodos] = useState(data)
+  const [todos, setTodos] = useState(
+    JSON.parse(localStorage.getItem('todos'))
+  )
 
   const removeTodo = (id) => {
     const obj = todos.filter(el => el.id !== id)
     setTodos(obj)
+    localStorage.setItem('todos', JSON.stringify(todos))
   }
 
   const addTodo = (text) => {
     const id = todos.length === 0 ? 1 : todos[todos.length-1].id + 1
     const obj = {'id': id, 'text':text, 'complete': false}
     setTodos([...todos, obj])
+    localStorage.setItem('todos', JSON.stringify(todos))
   }
+
+  // useEffect(() => {
+  //   localStorage.setItem('todos', JSON.stringify(todos))
+  // }, [todos])
 
   return (
     <div>
