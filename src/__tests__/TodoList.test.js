b019796b5aca04fb2029b6a01da6f66647780003
Yunju Chen <info@yunjuchen.net>
2020-08-22 20:36:45 -0700

    Update TodoList.test.js

--- src/__tests__/TodoList.test.js
+++ src/__tests__/TodoList.test.js
@@ -4,9 +4,7 @@ import TodoList from '../components/TodoList'
 import AddTodo from '../components/AddTodo'
 import Todo from '../components/Todo'
 
-
 describe("TodoList component", () => {
-
   it('AddTodo component is rendered', () => {
     const wrapper = shallow(<TodoList />);
     const counter = wrapper.find(AddTodo).exists();
@@ -18,5 +16,4 @@ describe("TodoList component", () => {
     const counter = wrapper.find(Todo).exists();
     expect(counter).toBe(true);
   })
-
 })
