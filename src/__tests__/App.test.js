011cf4a61e8dd4f5208dc0bfdf8052fc8887c972
Yunju Chen <info@yunjuchen.net>
2020-08-22 20:35:31 -0700

    Update App.test.js

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -3,16 +3,13 @@ import { shallow } from 'enzyme'
 import App from '../App'
 import TodoList from '../components/TodoList'
 
-
 describe("App component", () => {
-
   it('App title exists', () => {
     const wrapper = shallow(<App />);
     const title = wrapper.find('h1').text();
     expect(title).toBe('ToDo App');
   });
-
-
+  
   it('TodoList component is rendered', () => {
     const wrapper = shallow(<App />);
     const counter = wrapper.find(TodoList).exists();
